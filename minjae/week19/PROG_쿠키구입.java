d704e4d891ce2a87ac6bc56a4d76b78035e1f49f
Lee Min Jae <uiui96@naver.com>
2023-12-20 16:42:55 +0900

    PROG_쿠키구입.java

---
+++ minjae/week19/PROG_쿠키구입.java
@@ -0,0 +1,55 @@
+package week19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+public class PROG_쿠키구입 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+
+    static public int solution(int[] cookie) {
+
+
+        int leftSum = Arrays.stream(cookie).sum();
+        int rightSum = 0;
+        int mid = cookie.length;
+        int max = -1;
+        while(mid > 0){
+            mid--;
+
+            leftSum -= cookie[mid];
+            rightSum += cookie[mid];
+
+            int right = cookie.length-1;
+            int left = 0;
+
+            int tmpRightSum = rightSum;
+            int tmpLeftSum = leftSum;
+            while (true) {
+                if (tmpRightSum == tmpLeftSum) {
+                    max = Math.max(max, tmpRightSum);
+                    break;
+                }else if (tmpLeftSum < tmpRightSum){
+                    tmpRightSum -= cookie[right];
+                    right--;
+                }else{
+                    tmpLeftSum -= cookie[left];
+                    left++;
+                }
+            }
+        }
+
+
+        return max;
+    }
+    public static void main(String[] args) throws IOException {
+        System.out.println(solution(new int[] {1  ,2,  4  ,5}));
+    }
+
+
+}
